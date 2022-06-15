1decaea81041a27d1c73fffb37da09295b92822e
Jakub Josef Forman <me@jakubforman.eu>
2022-06-15 08:16:56 +0200

    Adding guide

--- README.md
+++ README.md
@@ -1,1 +1,239 @@
-# laravel-ftp-deploy
+# Laravel FTP Deploy
+
+Laravel FTP Deploy is a deployment guide for Laravel 9+ app. In guide, I describe how to implement simple ZIP deployment
+to shared hosting, like WEDOS, how to set up it for correct usage and how to set up CICD (GitHub Actions Workflow), for
+automatic deployment.
+
+**Testen on WEDOS Shared Hosting**
+
+- [Personal Website](https://jakubforman.eu/)
+- [Company Website](https://awwwesome.com/)
+- [Custom Mobile App Website](https://foode.cz/)
+
+Made by: Ing. Jakub Josef Forman (M.Sc.)
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/jakubforman)
+
+## Why I created this guide?
+
+1. Shared hosting is much cheaper than VPS.
+2. Shared hosting on WEDOS have lots of power and good setup for Laravel apps (maybe simple aps).
+3. ZIP upload is much faster than traditional file transport (basic Laravel project have about 24k files, but ZIP is
+   only one file).
+4. Used in my and our projects where we don't need VPS server.
+
+## Requirements
+
+- `PHP: 8.0.1+`
+- `ZipArchive` PHP extension on server
+- `zip` command in device shell (macOS or other unix / linux distribution)
+- `league/flysystem-ftp` installed on `composer require`
+
+## How it works
+
+1. Get all files of your project (without excludes files or folders).
+2. ZIP all together.
+3. Deploy ZIP to FTP why setup in Laravel traditional way.
+4. (Optionally) Migrate, fresh migrate and seed data (for seeder you need move `fakerphp/faker` from `require-dev` to `require`
+   dependencies).
+
+## What needs to do
+
+1. Setup FTP in your Laravel project in `config/filesystems.php`. You can set up many servers as you want. _Also you
+   need installed `composer require league/flysystem-ftp` (not in dev - for production, must be in production)_
+
+```php
+    'disks' => [
+        // ... other code
+        'production_ftp' => [
+            'driver' => 'ftp',
+            'host' => env('FTP_HOST'),
+            'username' => env('FTP_USERNAME'),
+            'password' => env('FTP_PASSWORD'),
+        ]
+    ]
+```
+
+2. Create new command for deploying in commands folder
+    - Copy original [Console DeployFTP command](src/DeployFTP.php) and paste it in `app/Console/Commands/DeployFTP.php`,
+      or you can reneame it
+3. Update deploy script
+    - Update `$excludes` as you want, if you need send empty folders to server like (storage/framework/sessions) you
+      also need update `getDeploymentCode()` function to create your folders.
+
+```php
+    /**
+     * @var array
+     */
+    protected array $excludes = [
+        '.idea',
+        '.phpunit.result.cache',
+        '.DS_Store',
+        '.editorconfig',
+        '.gitattributes',
+        '.git/*',
+        '.github/*',
+        '.scribe/*',
+        '.env',
+        'storage/app/*',
+        'storage/logs/*',
+        'storage/framework/cache/*',
+        'storage/framework/sessions/*',
+        'storage/framework/views/*',
+        '.htaccess', // only for FTP shared hosting, original still exist
+        'public/.htaccess', // only for FTP shared hosting, original still exist
+        'public/index.php' // only for FTP shared hosting, original still exist
+    ]; 
+```
+
+```php
+    /**
+     * return php deployment script code for unzipping archive and deleting old file
+     *
+     * Replacing content for WEDOS hosting
+     * @return string
+     */
+    private function getDeploymentCode()
+    {
+        return '
+            <?php
+            // vars
+            $archive = \'../' . static::ARCHIVE_NAME . '\';
+            $zip = new ZipArchive;
+
+            $res = $zip->open($archive);
+
+            if ($res === TRUE) {
+                // .... other code
+                echo "complete";
+            } else {
+                echo "error";
+            }';
+    }
+```
+
+4. Update deploy script 2 - If you don't want to replace composer php checker version, remove or set as return empty
+   string function `replaceMinPHPVersionOnHosting()` - this function is used for some hosting when you can't update PHP
+   to needed version. Updating `/vendor/composer/platform_check.php` this file.
+
+```php
+    /**
+     * Replace content for WEDOS hosting
+     * @return string
+     */
+    function replaceMinPHPVersionOnHosting(): string
+    {
+        return '
+              // REPLACE content for WEDOS!
+              $content = file_get_contents("../vendor/composer/platform_check.php");
+              $content = str_replace("80002", \'80001\', $content);
+              file_put_contents("../vendor/composer/platform_check.php", $content);
+        ';
+    }
+```
+
+5. Run Command for deploy
+    - For run you can use simple command (`php` or `sail` - if you run locally) `sail artisan deploy:ftp production_ftp`
+      `production_ftp` is defined in 1. Setup FTP!
+6. (optional) Automatic Migrate, Migrate Fresh and Seeder.
+    - Open `routes/web.php` and add this route. Route is protected by `MIGRATION_HASH` `env` variable which is generated
+      automatically before script zipping and sending files to FTP server and automatically added to `.env`.
+
+```php
+/**
+ * FTP Migration & seeder tool
+ */
+Route::get('migrate', function (Request $request) {
+    if ($request->has('hash')) {
+        if ($request->input('hash') == env('MIGRATION_HASH')) {
+            // WEDOS Artisan::call() fix
+            if (!defined('STDIN')) {
+                define('STDIN', fopen("php://stdin", "r"));
+            }
+
+            // generate command
+            $cmd = 'migrate';
+            if ($request->has('fresh')) {
+                $cmd = 'migrate:fresh';
+            }
+
+            // migrate
+            $migrateCode = Artisan::call($cmd, [
+                '--force' => true
+            ]);
+
+            // seed
+            $seedCode = null;
+            if ($request->has('seed')) {
+                $seedCode = Artisan::call('db:seed', [
+                    '--force' => true
+                ]);
+            }
+            return "ok [$migrateCode, $seedCode]";
+        }
+
+        return 'Hash mishmash!';
+    } else {
+        return 'You don\'t have permissions to do that!';
+    }
+});
+```
+
+## CICD Setup
+
+Basic setup for CICD
+
+1. Setup GitHub Actions Secrets (`DOT_ENV_PRODUCTION`, `ftp_username`, `ftp_password`), also you can set up server and
+   so on... This setup will be used in our [DeployFTP command](src/DeployFTP.php) for transfer ZIP file. _I setup FTP
+   login as separated ENV variables, because I don't need it in production `.env` file._
+   ![](github-secrets.png)
+2. Setup workflow - First setup .env file from your secret (you can also use another way), then deploy via
+   our [DeployFTP command](src/DeployFTP.php) to your destination. _You can visit own
+   simple [master.yml GitHub Workflow](src/master.yml)_
+
+```yml
+## Test, Build Release
+## ... Other code
+
+# copy .env from secrets
+- name: Create .env file from secrets
+  run: |
+    touch .env
+    printf '${{ secrets.DOT_ENV_PRODUCTION }}' > .env
+
+# deploy to FTP server
+- name: Deploy files to FTP
+  env:
+    FTP_HOST: xxxxxx.xx.xxxxxx.net
+    FTP_USERNAME: ${{ secrets.ftp_username }}
+    FTP_PASSWORD: ${{ secrets.ftp_password }}
+    ENV_DEPLOY: true
+  run: php artisan deploy:server production_ftp 
+```
+
+## WEDOS & Other shared hosting FIXS
+
+For error where you want to run `Artisan::call()` with `migrate` command, and see error like
+this `SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes`
+you can set custom Schema string length or update it manually in migration.
+
+```php
+<?php
+
+// ... other code
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    // ... other code
+    public function boot()
+    {
+        // ... other code
+        // FIX WEDOS ERROR
+        // SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+        Schema::defaultStringLength(766);
+    }
+}
+```
+
+
---
+++ src/DeployFTP.php
@@ -0,0 +1,338 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+/**
+ * Deploy to FTP
+ *
+ * requirements:
+ * - ZipArchive PHP extension on server
+ * - zip command in device shell
+ */
+class DeployFTP extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deploy:ftp {server} {--debug=1} ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deploy to server via FTP.';
+
+    /**
+     * @var string
+     */
+    protected string $disk = 'ftp';
+
+    /**
+     * Deploy path
+     * @var string
+     */
+    protected string $deployPath = 'domains/test.foode.cz';
+
+    /**
+     * Domain for run final scripts
+     * @var string
+     */
+    protected string $domain = 'test.foode.cz';
+
+    /**
+     * Random migration hash
+     * @var string
+     */
+    protected string $migrationHash;
+
+    /**
+     * @var array
+     */
+    protected array $before = [];
+
+    /**
+     * @var array
+     */
+    protected array $excludes = [
+        '.idea',
+        '.phpunit.result.cache',
+        '.DS_Store',
+        '.editorconfig',
+        '.gitattributes',
+        '.git/*',
+        '.github/*',
+        '.scribe/*',
+        '.env',
+        'storage/app/*',
+        'storage/logs/*',
+        'storage/framework/cache/*',
+        'storage/framework/sessions/*',
+        'storage/framework/views/*',
+        '.htaccess', // only for FTP shared hosting, original still exist
+        'public/.htaccess', // only for FTP shared hosting, original still exist
+        'public/index.php' // only for FTP shared hosting, original still exist
+    ];
+
+    /**
+     * code archive name
+     */
+    const ARCHIVE_NAME = 'deploy.zip';
+
+    /**
+     * php deployment script name
+     */
+    const SCRIPT_NAME = 'deploy.php';
+
+    /**
+     * setup needed instances
+     */
+    private function setup()
+    {
+        // Additional setup
+        $this->disk = $this->argument('server');
+        if (env('ENV_DEPLOY', false)) {
+            if (($key = array_search('.env', $this->excludes)) !== false) {
+                unset($this->excludes[$key]);
+            }
+        }
+
+        $this->migrationHash = Str::random(10); // generate random hash for client
+
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // setup
+        $this->setup();
+
+        // run
+        $this->info('------------------------------------------------------------------------');
+        $this->info('deploy the project');
+        $this->info('------------------------------------------------------------------------');
+        $this->info("");
+        $this->info("server:\t" . $this->argument('server'));
+        $this->info("");
+
+        $this->runBefore();
+        $this->createArchive();
+        $this->uploadFiles();
+        $this->runDeploymentScript();
+        // $this->cleanUpAfter();
+
+        $this->info("");
+        $this->info('------------------------------------------------------------------------');
+        $this->info('deployment done!');
+        $this->info('------------------------------------------------------------------------');
+        return 0;
+    }
+
+    /**
+     * Run exec commands before upload
+     * @return void
+     */
+    private function runBefore()
+    {
+        $this->info('- Running commands before deployment.');
+
+        foreach ($this->before as $cmd) {
+            $this->info('- ' . $cmd);
+            exec($cmd);
+        }
+    }
+
+    /**
+     * Run HTTP client GET method for unzip on server
+     * @return void
+     * @throws GuzzleException
+     */
+    private function runDeploymentScript()
+    {
+        $this->info('- Running deployment on server.');
+
+        // call the deployment script
+        $client = new Client();
+        $response = $client->get($this->domain . "/" . static::SCRIPT_NAME);
+        $responseMigrate = $client->get($this->domain . "/migrate?hash=$this->migrationHash&fresh&seed");
+
+        if ($this->option('debug')) {
+            $this->warn("\t- " . $response->getBody());
+            $this->warn("\t- " . $responseMigrate->getBody());
+        }
+
+        // delete the script itself
+        Storage::disk($this->disk)->delete($this->deployPath . "/public/" . static::SCRIPT_NAME);
+    }
+
+    /**
+     * Upload ZIP to FTP
+     * @return void
+     */
+    private function uploadFiles()
+    {
+        $this->info('- Uploading to server');
+
+        Storage::disk($this->disk)->put($this->deployPath . "/" . static::ARCHIVE_NAME, Storage::get(static::ARCHIVE_NAME));
+        Storage::disk($this->disk)->put($this->deployPath . "/public/" . static::SCRIPT_NAME, $this->getDeploymentCode());
+    }
+
+    /**
+     * clean up after completion of the uploading
+     */
+    private function cleanUpAfter()
+    {
+        $this->info('- Cleaning up after uploading.');
+
+        // delete created archive
+
+        if (Storage::exists(static::ARCHIVE_NAME)) {
+            Storage::delete(static::ARCHIVE_NAME);
+            $this->info('- Cleaned');
+        } else {
+            $this->error('- Not cleared');
+        }
+    }
+
+    /**
+     * Replace string hash in .env file for deploy and migrate on FTP
+     * @return void
+     */
+    private function createMigrationHashToEnvFile()
+    {
+        $env = file_get_contents(__DIR__ . "/../../../.env");
+        if (preg_match("/^MIGRATION_HASH=[a-zA-Z0-9]*$/m", $env)) {
+            $env = preg_replace("/^MIGRATION_HASH=[a-zA-Z0-9]*$/m", "MIGRATION_HASH=$this->migrationHash", $env);
+        } else {
+            $env = "$env\nMIGRATION_HASH=$this->migrationHash";
+        }
+        file_put_contents(__DIR__ . "/../../../.env", $env);
+    }
+
+    /**
+     * create zip archive
+     * @return $this
+     */
+    private function createArchive()
+    {
+        $this->info('- Building release zip.');
+
+        // replace string hash in .env file for deploy and migrate on FTP
+        $this->createMigrationHashToEnvFile();
+
+
+        // delete old archive
+        if (Storage::exists(static::ARCHIVE_NAME)) {
+            Storage::delete(static::ARCHIVE_NAME);
+        }
+
+        // create new archive
+        $this->createZipArchive();
+
+        return $this;
+    }
+
+    /**
+     * create the string to build the archive which will be uploaded
+     *
+     * @return void
+     */
+    private function createZipArchive()
+    {
+        $excludes = '';
+        if (is_array($this->excludes)) {
+            $excludes = "-x " . implode(" ", array_map(function ($v) {
+                    return "\"$v\"";
+                }, $this->excludes));
+        }
+
+        $rootPath = "./";
+        $zip = exec('zip -r ' . static::ARCHIVE_NAME . ' ' . $rootPath . ' ' . $excludes);
+        $mv = exec('mv ' . static::ARCHIVE_NAME . ' ' . storage_path('app'));
+
+        if ($this->option('debug')) {
+            $this->warn("\t- " . $zip);
+            $this->warn("\t- " . $mv);
+        }
+    }
+
+
+    /**
+     * Replace content for WEDOS hosting
+     * @return string
+     */
+    function replaceMinPHPVersionOnHosting(): string
+    {
+        return '
+              // REPLACE content for WEDOS!
+              $content = file_get_contents("../vendor/composer/platform_check.php");
+              $content = str_replace("80002", \'80001\', $content);
+              file_put_contents("../vendor/composer/platform_check.php", $content);
+        ';
+    }
+
+    /**
+     * return php deployment script code for unzipping archive and deleting old file
+     *
+     * Replacing content for WEDOS hosting
+     * @return string
+     */
+    private function getDeploymentCode()
+    {
+        return '
+            <?php
+            // vars
+            $archive = \'../' . static::ARCHIVE_NAME . '\';
+            $zip = new ZipArchive;
+
+            $res = $zip->open($archive);
+
+            if ($res === TRUE) {
+                $files = array();
+
+                for($i = 0; $i < $zip->numFiles; $i++) {
+                    $filename = $zip->getNameIndex($i);
+                    // if $filename not in destination / or whatever the logic is then
+                        $files[] = $filename;
+                }
+
+                $zip->extractTo(\'../\', $files);
+                $zip->close();
+
+                // create storage folders
+                if (!file_exists(\'../storage/framework/cache\') || !is_dir(\'../storage/framework/cache\')) {
+                    mkdir(\'../storage/framework/cache\');
+                }
+                if (!file_exists(\'../storage/framework/sessions\') || !is_dir(\'../storage/framework/sessions\')) {
+                    mkdir(\'../storage/framework/sessions\');
+                }
+                if (!file_exists(\'../storage/framework/testing\') || !is_dir(\'../storage/framework/testing\')) {
+                    mkdir(\'../storage/framework/testing\');
+                }
+                if (!file_exists(\'../storage/framework/views\') || !is_dir(\'../storage/framework/views\')) {
+                    mkdir(\'../storage/framework/views\');
+                }
+
+
+                // replace for hosting
+                ' . $this->replaceMinPHPVersionOnHosting() . '
+
+                echo "complete";
+            } else {
+                echo "error";
+            }';
+    }
+}
